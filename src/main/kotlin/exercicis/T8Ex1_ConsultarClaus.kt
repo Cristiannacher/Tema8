c917fd79cfce1d4f9177ef0635c9fb267066bfb0
Cristiannacher <alunac6050@ieselcaminas.org>
2023-01-25 13:26:41 +0100

    casa

--- src/main/kotlin/exercicis/T8Ex1_ConsultarClaus.kt
+++ src/main/kotlin/exercicis/T8Ex1_ConsultarClaus.kt
@@ -1,16 +1,34 @@
 package exercicis
 
 import redis.clients.jedis.Jedis
+import java.util.Scanner
 
-fun main(){
+val sc = Scanner(System.`in`)
+
+fun main() {
     val con = Jedis("89.36.214.106")
+    con.auth("ieselcaminas.ad")
     con.connect()
 
     val c = con.keys("*")  //c és un MutableSet
     var contador = 1
     for (s in c) {
-        println("$contador.- $s")
+        println("$contador.- $s ")
         contador++
+    }
+
+    var comprovador = true
+    var indice: Int
+    while (comprovador) {
+        println("Introdueix un numero 0 per acabar")
+        indice = sc.nextInt()
+        if (indice == 0) {
+            comprovador = false
+        } else {
+            println(c.elementAt(indice-1))
+
+        }
+
     }
 
     con.close()
