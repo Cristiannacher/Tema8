7a2fd6038e9958c5c917114fae769e255910a832
alu10486050 <alunac6050@ieselcaminas.org>
2023-01-25 20:54:28 +0100

    clase

--- src/main/kotlin/exercicis/T8Ex1_ConsultarClaus.kt
+++ src/main/kotlin/exercicis/T8Ex1_ConsultarClaus.kt
@@ -13,7 +13,7 @@ fun main() {
     val c = con.keys("*")  //c és un MutableSet
     var contador = 1
     for (s in c) {
-        println("$contador.- $s ")
+        println("$contador.- $s (${con.type(s)})")
         contador++
     }
 
@@ -22,11 +22,46 @@ fun main() {
     while (comprovador) {
         println("Introdueix un numero 0 per acabar")
         indice = sc.nextInt()
-        if (indice == 0) {
-            comprovador = false
-        } else {
-            println(c.elementAt(indice-1))
+        if (indice < 0 || indice > c.size) {
 
+        } else {
+            if (indice == 0) {
+                comprovador = false
+            } else {
+                when (con.type(c.elementAt(indice - 1))) {
+                    "string" -> {
+                        println("${c.elementAt(indice - 1)}:${con.get(c.elementAt(indice - 1))}")
+                    }
+
+                    "list" -> {
+                        println(c.elementAt(indice - 1))
+                        val ll = con.lrange(c.elementAt(indice - 1), 0, -1)
+                        for (e in ll)
+                            println(e)
+                    }
+
+                    "set" -> {
+                        println(c.elementAt(indice - 1))
+                        val s = con.smembers(c.elementAt(indice - 1))  // s és un MutableSet
+                        for (e in s)
+                            println(e)
+                    }
+
+                    "hash" -> {
+                        println(c.elementAt(indice - 1))
+                        val subcamps = con.hkeys(c.elementAt(indice - 1))
+                        for (subcamp in subcamps)
+                            println(subcamp + "--> " + con.hget(c.elementAt(indice - 1), subcamp))
+                    }
+
+                    "zset" -> {
+                        println(c.elementAt(indice - 1))
+                        val conjOrd = con.zrangeWithScores(c.elementAt(indice - 1), 0, -1)
+                        for (t in conjOrd)
+                            println(t.getElement() + " ---> " + t.getScore())
+                    }
+                }
+            }
         }
 
     }
---
+++ src/main/kotlin/exercicis/T8Ex2_ConsultaClausGrafica.kt
@@ -0,0 +1,108 @@
+package exercicis
+
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JTextField
+import javax.swing.JTextArea
+import javax.swing.DefaultListModel
+import javax.swing.JList
+import javax.swing.JScrollPane
+import java.awt.FlowLayout
+import java.awt.Color
+import redis.clients.jedis.Jedis
+
+import java.awt.EventQueue
+
+class EstadisticaRD : JFrame() {
+
+    val etTipClau= JLabel("Tipus:")
+    val tipClau= JTextField(8)
+    val contClau = JTextArea(8,15)
+    val con = Jedis("89.36.214.106")
+    val listModel = DefaultListModel<String>()
+    val llClaus = JList(listModel)
+
+    init {
+        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        setBounds(100, 100, 450, 450)
+        setLayout(FlowLayout())
+
+
+        llClaus.setForeground(Color.blue)
+        val scroll = JScrollPane(llClaus)
+        llClaus.setVisibleRowCount(20)
+
+        val scroll2 = JScrollPane(contClau)
+
+        add(scroll)
+        add(etTipClau)
+        add(tipClau)
+        add(scroll2)
+
+        setSize(600, 400)
+        setVisible(true)
+
+        inicialitzar()
+
+        llClaus.addListSelectionListener{valorCanviat()}
+
+    }
+    fun inicialitzar(){
+        con.auth("ieselcaminas.ad")
+        con.connect()
+        val c = con.keys("*")  //c és un MutableSet
+        for (s in c) {
+            listModel.addElement(s)
+        }
+    }
+
+    fun valorCanviat() {
+        con.auth("ieselcaminas.ad")
+        con.connect()
+        val c = con.keys("*")  //c és un MutableSet
+        tipClau.text = con.type(c.elementAt(llClaus.selectedIndex))
+        val indice = llClaus.selectedIndex
+
+        when (con.type(c.elementAt(indice))) {
+            "string" -> {
+                contClau.text = ("${c.elementAt(indice)}:${con.get(c.elementAt(indice))}")
+            }
+
+            "list" -> {
+                val ll = con.lrange(c.elementAt(indice ), 0, -1)
+                var text= ""
+                for (e in ll)
+                    text += "e \n"
+                contClau.text = text
+            }
+
+            "set" -> {
+                val s = con.smembers(c.elementAt(indice))  // s és un MutableSet
+                var text= ""
+                for (e in s)
+                    text += "e \n"
+                contClau.text = text
+            }
+
+            "hash" -> {
+                val subcamps = con.hkeys(c.elementAt(indice))
+                var text = ""
+                for (subcamp in subcamps)
+                    text += "$subcamp --> ${con.hget(c.elementAt(indice),subcamp)}"
+
+            }
+
+            "zset" -> {
+                val conjOrd = con.zrangeWithScores(c.elementAt(indice), 0, -1)
+                for (t in conjOrd)
+                    contClau.append(t.getElement() + " ---> " + t.getScore())
+            }
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    EventQueue.invokeLater {
+        EstadisticaRD().isVisible = true
+    }
+}
